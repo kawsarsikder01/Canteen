efbf910fda5e9e480e9035c6d96902e5f9d82418
kawsar <mdkawsarsikder01@gmail.com>
2023-05-17 23:12:35 +0600

    customers and product view section complete

--- admin/Template/layout_1/LTR/material/full/product_view.php
+++ admin/Template/layout_1/LTR/material/full/product_view.php
@@ -1,4 +1,24 @@
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/'.'collage_canteen'.'/'.'config.php');
+
+$id  = $_GET['id'];
+$productsJson = file_get_contents($adminSources.'products.json');
+$products = json_decode($productsJson);
+$productView;
+foreach($products as $product){
+    if($product->id == $id){
+        $productView = $product;
+        break;
+    }
+}
+
+?>
 <!DOCTYPE html>
+<html lang="en">
+<?php
+include_once($partialAdmin.'head.php');
+?>
+
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -6,7 +26,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Campus Canteen</title>
 
-    <!-- Global stylesheets -->
+    Global stylesheets
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
     <link href="../../../../global_assets/css/icons/icomoon/styles.min.css" rel="stylesheet" type="text/css">
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
@@ -14,16 +34,16 @@
     <link href="assets/css/layout.min.css" rel="stylesheet" type="text/css">
     <link href="assets/css/components.min.css" rel="stylesheet" type="text/css">
     <link href="assets/css/colors.min.css" rel="stylesheet" type="text/css">
-    <!-- /global stylesheets -->
+    /global stylesheets
 
-    <!-- Core JS files -->
+    Core JS files
     <script src="../../../../global_assets/js/main/jquery.min.js"></script>
     <script src="../../../../global_assets/js/main/bootstrap.bundle.min.js"></script>
     <script src="../../../../global_assets/js/plugins/loaders/blockui.min.js"></script>
     <script src="../../../../global_assets/js/plugins/ui/ripple.min.js"></script>
-    <!-- /core JS files -->
+    /core JS files
 
-    <!-- Theme JS files -->
+    Theme JS files
     <script src="../../../../global_assets/js/plugins/visualization/d3/d3.min.js"></script>
     <script src="../../../../global_assets/js/plugins/visualization/d3/d3_tooltip.js"></script>
     <script src="../../../../global_assets/js/plugins/forms/styling/switchery.min.js"></script>
@@ -44,11 +64,11 @@
     <script src="../../../../global_assets/js/demo_charts/pages/dashboard/light/bullets.js"></script>
 
 
-    <!--    For Data Tables-->
+       For Data Tables
     <script src="../../../../global_assets/js/demo_pages/datatables_basic.js"></script>
-    <!-- /theme JS files -->
+    /theme JS files
 
-</head>
+</head> -->
 
 <body>
 
@@ -497,21 +517,35 @@
 
         <!-- Content area -->
         <div class="content">
-            <div class="card mt-3">
+        <section id="customer-details">
+        <div class="customer-details">
+            <div class="image">
+                <img src="../../../../global_assets/images/<?=$productView->img?>" height="400px" width="400px" alt="">
+            </div>
+            <div class="details">
+                <h3><?=$productView->name?></h3>
+                <p>Price : <?=$productView->price?></p>
+                <p>Category :<?=$productView->category?></p>
+                <p>Type : <?=$productView->type?></p>
+                
+            </div>
+        </div>
+    </section>
+            <!-- <div class="card mt-3">
                 <div class="card-header header-elements-inline">
                     <h5 class="card-title">Customers</h5>
                     <a href="add_customer.html" class="btn btn-info legitRipple">Add Customer</a>
-                    <!-- <div class="header-elements">
+                     <div class="header-elements">
                         <div class="list-icons">
                             <a class="list-icons-item" data-action="collapse"></a>
                             <a class="list-icons-item" data-action="reload"></a>
                             <a class="list-icons-item" data-action="remove"></a>
                         </div>
-                    </div> -->
+                    </div> 
                 </div>
 
                 <div class="card-body">
-<!--                    The <code>DataTables</code> is a highly flexible tool, based upon the foundations of progressive enhancement, and will add advanced interaction controls to any HTML table. DataTables has most features enabled by default, so all you need to do to use it with your own tables is to call the construction function. Searching, ordering, paging etc goodness will be immediately added to the table, as shown in this example. <strong>Datatables support all available table styling.</strong>-->
+                    The <code>DataTables</code> is a highly flexible tool, based upon the foundations of progressive enhancement, and will add advanced interaction controls to any HTML table. DataTables has most features enabled by default, so all you need to do to use it with your own tables is to call the construction function. Searching, ordering, paging etc goodness will be immediately added to the table, as shown in this example. <strong>Datatables support all available table styling.</strong>
                 </div>
 
                 <div id="DataTables_Table_0_wrapper" class="dataTables_wrapper no-footer"><div class="datatable-header"><div id="DataTables_Table_0_filter" class="dataTables_filter"><label><span>Filter:</span> <input type="search" class="" placeholder="Type to filter..." aria-controls="DataTables_Table_0"></label></div><div class="dataTables_length" id="DataTables_Table_0_length"><label><span>Show:</span> <select name="DataTables_Table_0_length" aria-controls="DataTables_Table_0" class="select2-hidden-accessible" data-select2-id="1" tabindex="-1" aria-hidden="true"><option value="10" data-select2-id="3">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select><span class="select2 select2-container select2-container--default" dir="ltr" data-select2-id="2" style="width: auto;"><span class="selection"><span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-disabled="false" aria-labelledby="select2-DataTables_Table_0_length-g1-container"><span class="select2-selection__rendered" id="select2-DataTables_Table_0_length-g1-container" role="textbox" aria-readonly="true" title="10">10</span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span></label></div></div><div class="datatable-scroll"><table class="table datatable-basic dataTable no-footer" id="DataTables_Table_0" role="grid" aria-describedby="DataTables_Table_0_info">
@@ -567,7 +601,7 @@
                     </tr>
                 </tbody>
                 </table></div><div class="datatable-footer"><div class="dataTables_info" id="DataTables_Table_0_info" role="status" aria-live="polite">Showing 1 to 10 of 15 entries</div><div class="dataTables_paginate paging_simple_numbers" id="DataTables_Table_0_paginate"><a class="paginate_button previous disabled" aria-controls="DataTables_Table_0" data-dt-idx="0" tabindex="0" id="DataTables_Table_0_previous">←</a><span><a class="paginate_button current" aria-controls="DataTables_Table_0" data-dt-idx="1" tabindex="0">1</a><a class="paginate_button " aria-controls="DataTables_Table_0" data-dt-idx="2" tabindex="0">2</a></span><a class="paginate_button next" aria-controls="DataTables_Table_0" data-dt-idx="3" tabindex="0" id="DataTables_Table_0_next">→</a></div></div></div>
-            </div>
+            </div> -->
             <!-- /basic datatable -->
 
         </div>
@@ -585,7 +619,7 @@
 
             <div class="navbar-collapse collapse" id="navbar-footer">
 					<span class="navbar-text">
-						&copy; 2022 - 2023 <a href="#">Campus Canteen</a> by <a href="https://github.com/Prince-Costa" target="_blank">Prince Costa</a>
+						&copy; 2022 - 2023 <a href="#">Campus Canteen</a> by <a href="https://github.com/Prince-Costa" target="_blank">Kawsar Sikder</a>
 					</span>
 
                 <ul class="navbar-nav ml-lg-auto">
